e964dc0bb355bd575290401bbae64e648856ebcd
batuannghia96 <batuannghia96@gmail.com>
2019-12-10 21:40:48 +0700

    fix user

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -71,38 +71,41 @@
                         </tr>
                     @endforeach
                 @else
-                    <td>Không có đơn hàng nào</td>
+                    <td>Không có phản hồi nào nào</td>
                 @endif
                 </tbody>
             </table>
-            
-            <h5>Những đơn hàng của người dùng.</h5>
-            <table class="table">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Thời gian.</th>
-                    <th scope="col">Thanh toán.</th>
-                    <th scope="col">Trạng thái.</th>
-                    <th scope="col">Hành động.</th>
-                </tr>
-                </thead>
-                <tbody>
-                @if($user->orders()->count())
-                    @foreach($user->orders()->get() as $id => $value)
-                        <tr>
-                            <td>{{ $id }}</td>
-                            <td><a href="#">{{ date("H:i:s d/m/Y",strtotime($value->created_at)) }}</a></td>
-                            <td>{{ $value->total }}</td>
-                            <td>{{ $value->status }}</td>
-                            <td><a href="#">hiện thị</a></td>
-                        </tr>
-                    @endforeach
-                @else
-                    <td rowspan="4">Không có đơn hàng nào</td>
-                @endif
-                </tbody>
-            </table>
+            @if($user->role == \App\Models\User::CUSTOMER_ROLE)
+                <h5>Những đơn hàng của người dùng.</h5>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Khách hàng</th>
+                        <th scope="col">Thời gian.</th>
+                        <th scope="col">Thanh toán.</th>
+                        <th scope="col">Trạng thái.</th>
+                        <th scope="col">Hành động.</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @if($user->orders()->count())
+                        @foreach($user->orders()->get() as $id => $value)
+                            <tr>
+                                <td>{{ $id }}</td>
+                                <td><a href="#">{{ date("H:i:s d/m/Y",strtotime($value->created_at)) }}</a></td>
+                                <td>{{ date("H:i:s d/m/Y",strtotime($value->created_at)) }}</td>
+                                <td>{{ $value->total }}</td>
+                                <td>{{ $value->status }}</td>
+                                <td><a href="{{ route('orders.show', $value->id) }}">hiện thị</a></td>
+                            </tr>
+                        @endforeach
+                    @else
+                        <td rowspan="4">Không có đơn hàng nào</td>
+                    @endif
+                    </tbody>
+                </table>
+            @endif
         </div>
     </div>
 @endsection
