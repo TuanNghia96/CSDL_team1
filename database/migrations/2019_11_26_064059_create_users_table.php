15199f8962f1f1fe455d106fa855cac03f8cd8d6
batuannghia96 <batuannghia96@gmail.com>
2019-12-17 23:52:00 +0700

    add email to trigger

--- database/migrations/2019_11_26_064059_create_users_table.php
+++ database/migrations/2019_11_26_064059_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email', 100);
+            $table->string('email', 100)->unique();
             $table->string('name', 50);
             $table->string('avata_url', 255);
             $table->string('password', 255);
--- database/migrations/2019_12_16_155630_create_product_price_audit_table.php
+++ database/migrations/2019_12_16_155630_create_product_price_audit_table.php
@@ -15,6 +15,7 @@ class CreateProductPriceAuditTable extends Migration
     {
         Schema::create('product_price_audit', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('email', 100)->nullable();
             $table->unsignedInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->integer('price_before');
